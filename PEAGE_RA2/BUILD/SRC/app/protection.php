f8497a670c6425beff5f38105b2de4826ec6b249
assant <eden.assant1@ortmontreuil.ft>
2025-06-13 11:17:23 +0200

    Afin de bien protéger mon copyright je vais faire en sorte que si quelqu’un le retire le site plante . C’est ce qu’on appelle une condition protectionnelle. Donc j’ai decidé une fonction protection_Eden() cette fonction va retourner un  texte où il y aura écrit © Eden Assant . Cette fonction je l’ai crée dans un dossier app que j’ai mis dans le dossier SRC . Ensuite jai mis dans l’index.php  que si quelqu’un supprime ou modifie la fonction __protectionEden() dans app/protection.php, ça va faire planter le site.

---
+++ PEAGE_RA2/BUILD/SRC/app/protection.php
@@ -0,0 +1,5 @@
+<?php
+function __protectionEden() {
+     return '© Eden Assant';
+
+}
--- PEAGE_RA2/BUILD/SRC/index.php
+++ PEAGE_RA2/BUILD/SRC/index.php
@@ -37,20 +37,38 @@ session_start();
     <form id="form-libre">
       <h2>Recherche par champs libre</h2>
       <input name="libre" placeholder="Libre">
+        <input name="activite" placeholder="Activite">
       <button type="submit">Rechercher</button>
     </form>
 
     <form id="form-immat">
       <h2>Recherche par immatriculation</h2>
+        <input name="activite" placeholder="Activite">
       <input name="immat" placeholder="Immatriculation">
       <button type="submit">Rechercher</button>
     </form>
-    
+
   </section>
 
   <section id="result-zone"></section>
 
   <script src="js/script.js"></script>
+  <?php
+  require_once __DIR__ . '/app/protection.php';
+
+  if (!function_exists('__protectionEden')) {
+      die("Erreur critique : composant de sécurité manquant.");
+  }
+
+  $__eden__txt = __protectionEden();
+
+  if (strip_tags($__eden__txt) !== '© Eden Assant') {
+      die("Erreur fatale : modification interdite du copyright d’Eden.");
+  }
+
+  echo "<footer id='eden-copyright' style='text-align:center;color:gray;font-size:14px;margin-top:40px;'>$__eden__txt</footer>";
+  ?>
+
 </body>
 </html>
 
